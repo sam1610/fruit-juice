0118828271150ffe51bf227064466b0ceac9138f
Thalius <yamil.abugattas@gmail.com>
2015-02-20 07:44:10 -0500

    Add user database class (userdb)

---
+++ lib/DB/userdb.py
@@ -0,0 +1,7 @@
+from google.appengine.ext import db
+
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    password = db.StringProperty(required = True)
+    email = db.EmailProperty()
+    created = db.DateTimeProperty(auto_now_add = True)
