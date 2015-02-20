e296ecdc42bdbf1e0d49e84e0bafbf09f92575cd
Thalius <yamil.abugattas@gmail.com>
2015-02-20 12:36:41 -0500

    Add space between functions.

--- lib/utils.py
+++ lib/utils.py
@@ -13,11 +13,14 @@ def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+
 def hash_str(s):
     return hmac.new(SECRET, s).hexdigest() #Por defecto usa md5, cambiar a sha-1 o sha256 para uso real.
 
+
 def make_secure_val(s):
     return "%s|%s" % (s, hash_str(s))
+
 
 def check_secure_val(h):
     if h:
@@ -25,8 +28,10 @@ def check_secure_val(h):
         if h == make_secure_val(val):
             return val
 
+
 def make_salt():
     return ''.join(random.choice(string.letters) for x in xrange(5))
+
 
 def make_pw_hash(name, pw, salt = None):
     if not salt:
@@ -34,17 +39,21 @@ def make_pw_hash(name, pw, salt = None):
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s,%s' % (h, salt)
 
+
 def valid_pw(name, pw, h):
     salt = h.split(',')[1]
     return h == make_pw_hash(name, pw, salt)
+
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
 
+
 PASS_RE = re.compile(r"^.{3,20}$")
 def valid_password(password):
     return password and PASS_RE.match(password)
+    
 
 EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
