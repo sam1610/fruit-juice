69a3a87171ae9fb3216f90877b24fcc2ae764c29
Thalius <yamil.abugattas@gmail.com>
2015-02-20 11:10:17 -0500

    Add the SignupHandler to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from handlers import * #Solo hay handlers, por lo que no causa problemas con el namespace.
 
 app = webapp2.WSGIApplication([
-    ('/', FrontHandler)
+    ('/', FrontHandler),
+    ('/signup/?', SignupHandler)
 ], debug=True)
