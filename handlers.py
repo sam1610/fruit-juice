a9607315602dce261a3a24628772e050fff3d2b8
Thalius <yamil.abugattas@gmail.com>
2015-02-20 07:58:12 -0500

    Add jinja2 variables

--- handlers.py
+++ handlers.py
@@ -3,6 +3,9 @@ import webapp2, jinja2
 from lib import utils
 from lib.DB import userdb
 
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
