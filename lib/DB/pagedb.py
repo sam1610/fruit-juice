a41d586ee3a26a6e974468494666489e8642ae72
Thalius <yamil.abugattas@gmail.com>
2015-02-21 13:46:59 -0500

    Add pagedb file

---
+++ lib/DB/pagedb.py
@@ -0,0 +1,6 @@
+from google.appengine.ext import db
+
+class Page(db.Model):
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
