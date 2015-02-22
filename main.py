d00e711c6e2408bbc955b6478f83eb364795395e
Thalius <yamil.abugattas@gmail.com>
2015-02-21 21:00:49 -0500

    Change pages regular expresion

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import webapp2
 import handlers
 
-PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
+PAGE_RE = r'((?:[a-zA-Z0-9_-]+/?)*)'
 
 app = webapp2.WSGIApplication([
     ('/', handlers.FrontHandler),
     ('/signup/?', handlers.SignUpHandler),
     ('/login/?', handlers.LoginHandler),
-    ('/_edit' + PAGE_RE, handlers.EditPage),
-    (PAGE_RE, handlers.WikiPage)
+    ('/_edit/' + PAGE_RE, handlers.EditPage),
+    ('/' + PAGE_RE, handlers.WikiPage)
 ], debug=True)
