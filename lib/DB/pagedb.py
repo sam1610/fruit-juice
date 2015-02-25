e69cd06631238e0f6744acc7b7fd59c7162b1f93
Thalius <yamil.abugattas@gmail.com>
2015-02-25 12:02:43 -0500

    Add a ReferenceProperty to store multiple contents

--- lib/DB/pagedb.py
+++ lib/DB/pagedb.py
@@ -1,6 +1,23 @@
 from google.appengine.ext import db
 
 class Page(db.Model):
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
     last_modified = db.DateTimeProperty(auto_now = True)
+
+    def get_by_v_number(self, v_num):
+    	return self.pages.filter('version =', v_num).get()
+
+    def current_v(self):
+    	c_version = 0
+    	for page in self.pages:
+    		if page.version > c_version:
+    			c_version = page.version
+    	return c_version
+
+
+class PageContent(db.Model):
+	page = db.ReferenceProperty(Page,
+								collection_name = pages)
+	
+	created = db.DateTimeProperty(auto_now_add = True)
+	content = db.TextProperty(required = True)
+	version = db.IntegerProperty(required = True)
