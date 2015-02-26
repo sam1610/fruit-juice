baa8a4b7c1e03e45b5fa89e60587c549c7024118
Thalius <yamil.abugattas@gmail.com>
2015-02-25 21:08:38 -0500

    Change handlers according to the course's instructions

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import webapp2
 import handlers
 
-PAGE_RE = r'((?:[a-zA-Z0-9_-]+/?)*)'
-
-app = webapp2.WSGIApplication([
-    ('/', handlers.FrontHandler),
-    ('/signup/?', handlers.SignUpHandler),
-    ('/login/?', handlers.LoginHandler),
-    ('/logout/?', handlers.LogoutHandler),
-    ('/_edit/?', handlers.EditPage),
-    ('/_edit/' + PAGE_RE, handlers.EditPage),
-    ('/' + PAGE_RE, handlers.WikiPage)
-], debug=True)
+PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
+app = webapp2.WSGIApplication([('/signup', handlers.SignUpHandler),
+                               ('/login', handlers.LoginHandler),
+                               ('/logout', handlers.LogoutHandler),
+                               ('/_edit' + PAGE_RE, handlers.EditPage),
+                               (PAGE_RE, handlers.WikiPage),
+                               ],
+                              debug=True)
