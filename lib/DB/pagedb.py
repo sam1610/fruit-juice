11de182bb2dc757a7f27fc6826349e44a861bc68
Thalius <yamil.abugattas@gmail.com>
2015-02-28 18:48:41 -0500

    Replace tabs with spaces

--- lib/DB/pagedb.py
+++ lib/DB/pagedb.py
@@ -4,22 +4,22 @@ class Page(db.Model):
     last_modified = db.DateTimeProperty(auto_now = True)
 
     def get_content(self, v_num=None):
-    	if not v_num or not str(v_num).isdigit():
+        if not v_num or not str(v_num).isdigit():
             v_num = self.current_v()
         return self.pages.filter('version =', int(v_num)).get()
 
     def current_v(self):
-    	c_version = 0
-    	for page in self.pages:
-    		if page.version > c_version:
-    			c_version = page.version
-    	return c_version
+        c_version = 0
+        for page in self.pages:
+            if page.version > c_version:
+                c_version = page.version
+        return c_version
 
 
 class PageContent(db.Model):
-	page = db.ReferenceProperty(Page,
-								collection_name = "pages")
-	
-	created = db.DateTimeProperty(auto_now_add = True)
-	content = db.TextProperty(required = True)
-	version = db.IntegerProperty(required = True, default = 0)
+    page = db.ReferenceProperty(Page,
+                                collection_name = "pages")
+    
+    created = db.DateTimeProperty(auto_now_add = True)
+    content = db.TextProperty(required = True)
+    version = db.IntegerProperty(required = True, default = 0)
