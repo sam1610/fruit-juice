eb3611e5bf0721c050092247b45ebc7ca141ce2a
Thalius <yamil.abugattas@gmail.com>
2015-03-07 07:39:12 -0500

    Add comments to pagedb.py

--- lib/DB/pagedb.py
+++ lib/DB/pagedb.py
@@ -1,8 +1,13 @@
 from google.appengine.ext import db
 
+
+#This function creates a key to assign a parent to each instance
 def page_key(group = 'pages'):
     return db.Key.from_path('wiki', group)
 
+
+#Page class, with multiple versions per page
+#One to many relation with PageContent
 class Page(db.Model):
     last_modified = db.DateTimeProperty(auto_now = True)
 
@@ -11,13 +16,17 @@ class Page(db.Model):
             return self.current_v()
         return PageContent.get_by_id(ids=int(v_num), parent=page_key())
 
+    #Return the latest version
     def current_v(self):
         versions = self.sorted_versions()
         if versions:
             return self.sorted_versions()[0]
 
+    #Return list of versions sorted from old to new
     def sorted_versions(self):
-        versions = sorted(self.pages, key=lambda page: page.created, reverse=True)
+        versions = sorted(
+            self.pages, key=lambda page: page.created, reverse=True
+            )
         return versions
 
 
