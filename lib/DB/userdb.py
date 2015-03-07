8137a1d1a9dbed31dbba53c6dec1258da7785f6f
Thalius <yamil.abugattas@gmail.com>
2015-03-07 07:40:15 -0500

    Add comments to userdb.py

--- lib/DB/userdb.py
+++ lib/DB/userdb.py
@@ -1,5 +1,7 @@
 from google.appengine.ext import db
 
+
+#User class, contains basic information added when the account is created
 class User(db.Model):
     username = db.StringProperty(required = True)
     password = db.StringProperty(required = True)
