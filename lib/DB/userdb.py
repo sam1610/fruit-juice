f3b03f8187bc9bdd44116642bb455343f05f4eba
Thalius <yamil.abugattas@gmail.com>
2015-03-07 10:02:12 -0500

    Fix whitespace

--- lib/DB/userdb.py
+++ lib/DB/userdb.py
@@ -3,7 +3,7 @@ from google.appengine.ext import db
 
 #User class, contains basic information added when the account is created
 class User(db.Model):
-    username = db.StringProperty(required = True)
-    password = db.StringProperty(required = True)
+    username = db.StringProperty(required=True)
+    password = db.StringProperty(required=True)
     email = db.EmailProperty()
-    created = db.DateTimeProperty(auto_now_add = True)
+    created = db.DateTimeProperty(auto_now_add=True)
